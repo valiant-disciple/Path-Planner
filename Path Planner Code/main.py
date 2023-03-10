3fcb5accb77093fbf3777e4e456f44a934790198
valiant-disciple <127472799+valiant-disciple@users.noreply.github.com>
2023-03-10 14:39:38 +0530

    Update main.py

--- Path Planner Code/main.py
+++ Path Planner Code/main.py
@@ -5,21 +5,19 @@ Created on Fri Mar  3 17:23:33 2023
 @author: Arya
 """
 
-import numpy as np
-import cv2
-from PIL import Image
+#import numpy as np
+#import cv2
+#from PIL import Image
 import Algorithm
 import ObjectDetection 
 
-data = ObjectDetection.Starter()
-print(data.dtype)
+data = ObjectDetection.Starter() #Object Detection starts here, returns pixel map of img.
+# print(data.dtype)
 
 # image = Image.open('pixel_map.png')
 # data = np.array(image)
 
-myAlgo = Algorithm.A_Star_Algo(data)
-protocol = myAlgo.algo_exec()
+myAlgo = Algorithm.A_Star_Algo(data) #Runs the path-finding algorithm.
+protocol = myAlgo.algo_exec() #Text Protocol for motion.
 
-'''Main should recieve path_points and then we open the pixel map here'''
-# print(myAlgo.closed_list)
-# print(myAlgo.open_list)
+"""Call your functions for the motion of the car here"
