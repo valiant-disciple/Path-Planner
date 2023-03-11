aa206071c12732760d5d54ed3919ce43ddd8e15b
akulsylvania <121822281+akulsylvania@users.noreply.github.com>
2023-03-12 00:14:11 +0530

    Update main.py
    added socket

--- Path Planner Code/main.py
+++ Path Planner Code/main.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Mar  3 17:23:33 2023
-
 @author: Arya
 """
+import socket
 
 #import numpy as np
 #import cv2
@@ -20,4 +20,15 @@ data = ObjectDetection.Starter() #Object Detection starts here, returns pixel ma
 myAlgo = Algorithm.A_Star_Algo(data) #Runs the path-finding algorithm.
 protocol = myAlgo.algo_exec() #Text Protocol for motion.
 
-"""Call your functions for the motion of the car here"
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostbyname(socket.gethostname()), 1234))
+s.listen(5)
+txt=socket.gethostbyname(socket.gethostname())
+while True:
+   
+    clientsocket, address = s.accept()
+    print(f"Connection established.")
+    clientsocket.send(bytes(protocol,"utf-8"))
+    print(txt)
